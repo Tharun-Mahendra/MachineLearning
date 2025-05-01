806a14e30abab57bb3da2d8075a62939cb422e4f
TharunMahendra <tharunmahendra.pathakota@gmail.com>
2025-05-01 15:35:38 +0530

    Non-LinearRegressionModel

---
+++ 9.RandomForestRegression/code.py
@@ -0,0 +1,26 @@
+# Importing Libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#let's import the dataset
+data=pd.read_csv(r"C:\Users\TharunMahendra\NIT\6.Algorithms\1.Regression\Employee-Salary.csv")
+
+#lets divide the dataset into independent and dependent variables
+x=data.iloc[:,1:2].values
+y=data.iloc[:,2].values
+
+from sklearn.ensemble import RandomForestRegressor
+model=RandomForestRegressor(random_state=0)
+model.fit(x, y)
+
+
+prediction=model.predict([[6.5]])
+prediction
+
+# Visualising the results
+plt.scatter(x, y, color = 'red')
+plt.plot(x, model.predict(x), color = 'blue')
+plt.title('Truth or Bluff (Random Forest)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
